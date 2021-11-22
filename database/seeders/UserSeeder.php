83895cd6c560b5fa469be6dbbfd9df9441908537
Pedro Gabriel <pedrogab96@gmail.com>
2021-11-22 09:54:46 -0300

    adding the model to user seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,7 +15,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        User::create([
             'name' => 'Administrador',
             'email' => 'administrador@rits.com',
             'password' => '$2y$10$N69W5N.uTt8NeQJ./h4AIeHVzPuAyXoeTiUkNsmQZxVSbrsxP.Jlq',
