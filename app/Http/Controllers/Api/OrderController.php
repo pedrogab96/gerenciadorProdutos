a5393ce9976135171fd767aa402234a23cb2c437
Pedro Gabriel <pedrogab96@gmail.com>
2021-11-22 09:08:26 -0300

    creating the crud and the list of orders for the api

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -14,18 +14,36 @@ class OrderController extends Controller
         $this->orderService = $orderService;
     }
 
-    public function getAllOrders()
+    public function index()
     {
-        $orders = $this->orderService->getAllOrders();
+        $orders = $this->orderService->getOrders();
+        return response()->json($orders);
     }
 
-    public function getOrderById($id)
+    public function show(Request $request, $order_id)
     {
-        $order = $this->orderService->getOrderById($id);
+        $customer_id = $request->customer_id;
+        $order = $this->orderService->show($order_id);
+        return response()->json($order);
     }
 
-    public function createOrder(Request $request)
+    public function create(Request $request)
     {
-        $order = $this->orderService->createOrder($request);
+        $data = $request->except('_token', '_method');
+        $this->orderService->create($data);
+        return response()->json(['message' => 'Order created successfully'], 200);
+    }
+
+    public function update(Request $request, $order_id)
+    {
+        $data = $request->except('_token', '_method');
+        $this->orderService->update($data, $order_id);
+        return response()->json(['message' => 'Order updated successfully'], 200);
+    }
+
+    public function delete(Request $request, $order_id)
+    {
+        $this->orderService->delete($order_id);
+        return response()->json(['message' => 'Order deleted successfully'], 200);
     }
 }
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -6,13 +6,47 @@ use App\Models\Order;
 
 class OrderService
 {
-    public function getAll()
+    public function getOrders()
     {
-        return Order::all();
+        $order = Order::join('customers', 'customers.id', '=', 'orders.customer_id')
+            ->select('orders.*', 'customers.name as customer_name')
+            ->get();
+        return $order;
     }
 
-    public function create(array $data)
+    public function show($order_id)
     {
-        return Order::create($data);
+        $order = Order::find($order_id);
+        $order->products = $order->products()->get();
+        return $order;
+    }
+
+    public function create($params)
+    {
+        $productIds = collect($params['products'])->pluck('id')->toArray();
+        $order = new Order();
+        $order->customer_id = $params['customer_id'];
+        $order->status = $params['status'] ?? 'pending';
+        $order->save();
+        $order->products()->attach($productIds);
+        $total_price = $order->products()->sum('price');
+        $order->total_price = $total_price;
+        $order->save();
+    }
+
+    public function update($params, $order_id)
+    {
+        $productIds = collect($params['products'])->pluck('id')->toArray();
+        $order = Order::find($order_id);
+        $order->status = $params['status'];
+        $order->save();
+        $order->products()->sync($productIds);
+    }
+
+    public function delete($order_id)
+    {
+        $order = Order::find($order_id);
+        $order->products()->detach();
+        $order->delete();
     }
 }
