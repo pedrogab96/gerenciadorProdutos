0b8ce0db53275b3f6d435e1a2a7db7d389c409f7
Pedro Gabriel <pedrogab96@gmail.com>
2021-11-22 16:06:30 -0300

    added product creation and ordering in seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(CustomerSeeder::class);
+
+        $products = Product::factory()->count(10)->create();
+        Order::factory()->count(10)->create()->each(function ($order) use ($products) {
+            $order->products()->attach($products->random(rand(1, 3))->pluck('id'));
+            $order->total_price = $order->products->sum('price');
+            $order->save();
+        });
     }
 }
